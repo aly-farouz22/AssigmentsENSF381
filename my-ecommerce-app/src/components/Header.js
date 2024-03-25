17372b5277f36b50bc8541933ee24fd40aac8524
Aly <aly.farouz@ucalgary.ca>
2024-03-25 13:18:03 -0600

    HomePage Done

--- my-ecommerce-app/src/components/Header.js
+++ my-ecommerce-app/src/components/Header.js
@@ -5,13 +5,13 @@ function Header() {
   return (
     <header>
       <div className="logo">
-        <img src="/logo.png" alt="Company Logo" /> 
+        <img src="images/logo.png" alt="Company Logo" style={{width: '50px'}} /> 
       </div>
       <div className="company-name">
-        <h1>Company Name</h1>
+        <h1 style={{ textAlign: 'right' }}>Company Name</h1>
       </div>
       <nav>
-        <ul>
+        <ul style={{ display: 'flex', justifyContent: 'space-between', listStyle: 'none', padding: 0 }}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/products">Products</Link></li>
           <li><Link to="/login">Login</Link></li>
